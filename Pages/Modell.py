8c1b83192cb74fd1399c0b0993e695921cfa90b5
Steet1Net <122820888+Steet1Net@users.noreply.github.com>
2024-05-10 11:11:24 +0200

    Update Modell.py
    updates Modell.py

--- Pages/Modell.py
+++ Pages/Modell.py
@@ -1,43 +1,61 @@
 import traceback
 import streamlit as st
 from sqlalchemy import text
-from Dashboard import showSidebar
-showSidebar()
+from Dashboard import db, show_sidebar
 
-db = st.connection('mysql', type='sql')
-typen = db.query("SELECT fahrzeug_typ, id FROM fahrzeugtypen")
-marken = db.query("SELECT marke, id FROM marken")
+st.set_page_config(
+    page_icon="🚧",
+    layout="wide"
+)
+show_sidebar()
+if st.query_params == {}:
+    st.query_params["typ"] = None
 
 
-st.title("Neues Modell hinzufügen")
-with st.form("Neues Modell", clear_on_submit=True):
-    typ = st.selectbox("Fahrzeugtyp", typen, index=None)
+def neues_modell():
+    typen = db.query("SELECT fahrzeug_typ, id FROM fahrzeugtypen")
+    marken = db.query("SELECT marke, id FROM marken")
 
-    col1, col2 = st.columns([1, 2])
-    with col1:
-        marke = st.selectbox("Marke", marken["marke"], index=None)
-    with col2:
-        name = st.text_input("Name")
-    gewicht = st.number_input("Gewicht in Kg", value=0)
-    elektrisch = st.checkbox("Elektrisch")
-    beschreibung = st.text_area("Beschreibung")
-    if st.form_submit_button("Modell hinzufügen"):
-        if name != "":
-            try:
-                if marke != "":
-                    if marke + " " not in name:
-                        name = marke + " " + name
-                s = db.session
-                typ_id = db.query("SELECT id FROM fahrzeugtypen WHERE fahrzeug_typ ='" + str(typ) + "'", ttl=0)["id"][0]
-                query = text(
-                    "INSERT INTO fahrzeugmodelle(typ_id, fahrzeug_modell, gewicht, beschreibung, elektrisch) VALUES ("
-                    ":typ_id, :name, :gewicht, :beschreibung, :elektrisch)")
-                s.execute(query, {"typ_id": typ_id, "name": name, "gewicht": gewicht, "beschreibung": beschreibung,
-                                  "elektrisch": elektrisch})
-                s.commit()
-                s.close()
-                st.success("Modell hinzugefügt")
-            except Exception as e:
-                st.error(traceback.format_exc())
+    st.title("Neues Modell hinzufügen")
+    with st.form("Neues Modell", clear_on_submit=True):
+        if st.query_params["typ"] is not None:
+            filtered_typen = typen[typen["fahrzeug_typ"] == str(st.query_params["typ"])]
+            if not filtered_typen.empty:
+                typ = st.selectbox("Fahrzeugtyp", typen["fahrzeug_typ"], index=int(filtered_typen.index[0]))
+            else:
+                typ = st.selectbox("Fahrzeugtyp", typen["fahrzeug_typ"], index=None)
         else:
-            st.error("Fehlerhafte eingabe!")
+            typ = st.selectbox("Fahrzeugtyp", typen["fahrzeug_typ"], index=None)
+
+        col1, col2 = st.columns([1, 2])
+        with col1:
+            marke = st.text_input("Marke")
+        with col2:
+            name = st.text_input("Name")
+        gewicht = st.number_input("Gewicht in Kg", value=0)
+        elektrisch = st.checkbox("Elektrisch")
+        beschreibung = st.text_area("Beschreibung")
+        if st.form_submit_button("Modell hinzufügen"):
+            if name != "" and marke != "":
+                try:
+                    if marke != "":
+                        name = marke + " " + name
+                    session = db.session
+                    typ_id = \
+                        db.query("SELECT id FROM fahrzeugtypen WHERE fahrzeug_typ ='" + str(typ) + "'", ttl=0)["id"][0]
+                    query = text(
+                        "INSERT INTO fahrzeugmodelle(typ_id, fahrzeug_modell, gewicht, beschreibung, elektrisch) "
+                        "VALUES (:typ_id, :name, :gewicht, :beschreibung, :elektrisch)")
+                    session.execute(query,
+                                    {"typ_id": typ_id, "name": name, "gewicht": gewicht, "beschreibung": beschreibung,
+                                     "elektrisch": elektrisch})
+                    session.commit()
+                    session.close()
+                    st.success("Modell hinzugefügt")
+                except Exception as e:
+                    st.error(traceback.format_exc())
+            else:
+                st.error("Fehlerhafte eingabe!")
+
+
+neues_modell()
